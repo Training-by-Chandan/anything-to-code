30338fb2ac7d11a0dd81a5c08384af5c0a8bf212
Chandan Gupta Bhagat <chandan.bhagat@outlook.com>
2021-12-02 16:44:31 +0545

    console project added

---
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApp
+{
+    public class Programs
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hi this is our C# Class");
+            Console.ReadLine();
+        }
+    }
+}
