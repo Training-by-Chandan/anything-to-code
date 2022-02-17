56ea602f37742bfb247d5c20ff5cfee6b127de6a
Chandan Gupta Bhagat <chandan.bhagat@outlook.com>
2022-02-17 21:28:25 +0545

    Web project added

--- DesktopApp/DesktopApp/ViewModels/LoginViewModel.cs
+++ DesktopApp/DesktopApp/ViewModels/LoginViewModel.cs
@@ -5,4 +5,11 @@
         public string Username { get; set; }
         public string Password { get; set; }
     }
+
+    public class RegistrationViewModel
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
+    }
 }
---
+++ WebApp/WebApp/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Test()
+        {
+            return View("Contact");
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        [Authorize]
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
