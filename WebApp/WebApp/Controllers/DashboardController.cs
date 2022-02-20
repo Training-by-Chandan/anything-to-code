17e60f272890e549b642f0c086ec8819fb020bdd
Chandan Gupta Bhagat <chandan.bhagat@outlook.com>
2022-02-20 18:08:18 +0545

    partial page, model binding in views and partial views

---
+++ WebApp/WebApp/Controllers/DashboardController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApp.ViewModels;
+
+namespace WebApp.Controllers
+{
+    public class DashboardController : Controller
+    {
+        // GET: Dashboard
+        public ActionResult Index()
+        {
+            var model = GenerateDashboardData();
+
+            return View(model);
+        }
+
+        private DashboardViewModel GenerateDashboardData()
+        {
+            var data = new DashboardViewModel();
+            data.Widgets.Add(new Widget() { WidgetType="success", Icon = "copy", Title = "Sales", Numbers = 100 });
+            data.Widgets.Add(new Widget() { WidgetType="info", Icon = "edit", Title = "purchase", Numbers = 100 });
+            var displayBox = new List<DisplayBoxViewModel>();
+            displayBox.Add(new DisplayBoxViewModel() { BoxType = "danger", Icon = "copy", Body = "50% in 30 days", Numbers = "2,990", Progress = 50, Title = "Purchase" });
+            displayBox.Add(new DisplayBoxViewModel() { BoxType = "success", Icon = "copy", Body = "80% in 30 days", Numbers = "23,990", Progress = 80, Title = "Sales" });
+
+            data.DisplayBoxes = displayBox;
+            return data;
+        }
+    }
+}
---
+++ WebApp/WebApp/ViewModels/DisplayBoxViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApp.ViewModels
+{
+    public class DisplayBoxViewModel
+    {
+        public string BoxType { get; set; }
+        public string Icon { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+        public string Numbers { get; set; }
+        public int Progress { get; set; }
+    }
+
+    public class DashboardViewModel
+    {
+        public DashboardViewModel()
+        {
+            this.DisplayBoxes = new List<DisplayBoxViewModel>();
+            this.Widgets = new List<Widget>();
+        }
+        public List<DisplayBoxViewModel> DisplayBoxes { get; set; }
+       public List<Widget> Widgets { get; set; }
+    }
+
+    public class Widget
+    {
+        public string Icon { get; set; }
+        public string WidgetType { get; set; }
+        public string Title { get; set; }
+        public double Numbers { get; set; }
+    }
+}
